9e37cc758ed97f52f43603c9f354486969f2575e
fusionjack <dogfight60-fusionjack@yahoo.de>
2015-11-27 23:36:52 +0100

    Make it compatible to About dialog's system updates

--- Android.mk
+++ Android.mk
@@ -6,6 +6,7 @@ ifneq ($(ANDROID_BUILD_TOP),)
 endif
 
 LOCAL_MODULE_TAGS := optional
+LOCAL_PRIVILEGED_MODULE := true 
 LOCAL_PACKAGE_NAME := CyanpopOTA
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
